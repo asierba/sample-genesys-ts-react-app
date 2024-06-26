090926846d0afe8fdcbe95b265a76e945a2c506e
Asier Barrenetxea <abarrenetxea@wayfair.com>
2024-06-26 16:29:59 +0200

    Show error if library doesnt work on top of App

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,21 @@ import './App.css';
 import platformClient from 'purecloud-platform-client-v2';
 
 console.log('platformClient', platformClient);
-console.log('platformClient.ApiClient.instance', platformClient.ApiClient.instance);
+
+let error: any = null;
+
+try {
+  console.log('platformClient.ApiClient.instance', platformClient.ApiClient.instance);
+} catch (e) {
+  console.error('platformClient.ApiClient.instance', e);
+  error = e;
+}
 
 
 function App() {
   return (
     <div className="App">
+      {error && <h1>{error.toString()}</h1> }
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
